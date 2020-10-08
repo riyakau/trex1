1311c36911a8ffd45e215999a7b70a97c31ffe11
riyakau <72134103+riyakau@users.noreply.github.com>
2020-10-08 20:01:50 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var ground, invisibleGround, groundImage;
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
-var score=0;
+var score=1;
 
 var gameOver, restart;
 
