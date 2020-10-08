24a6466cf6b65ebd5088c197257db44211c01b33
riyakau <72134103+riyakau@users.noreply.github.com>
2020-10-08 20:00:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -53,7 +53,7 @@ function setup() {
   restart = createSprite(300,140);
   restart.addImage(restartImg);
   
-  gameOver.scale = 0.5;
+  gameOver.scale = 1;
   restart.scale = 0.5;
 
   gameOver.visible = false;
